0c783389b2b1a9d45c7bb3fdd364917005f82212
zachary-kiekover <58746702+zachary-kiekover@users.noreply.github.com>
2021-06-23 14:24:34 -0400

    Updating rule Requiring modules to define a defualt export to Off

--- packages/eslint-config-airbnb-base/rules/imports.js
+++ packages/eslint-config-airbnb-base/rules/imports.js
@@ -160,7 +160,7 @@ module.exports = {
     // **** We had a good number of violations of this rule, but seems easy enough to comply, its simple to fix but could be 100s of files maybe a warn?
     // Require modules with a single export to use a default export
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
-    'import/prefer-default-export': 'error',
+    'import/prefer-default-export': 'off',
 
     // Restrict which files can be imported in a given folder
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-restricted-paths.md
